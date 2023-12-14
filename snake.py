74117a64fbe955048d1436b36ac5b5137736c370
Nico <nicolas.houbouyan@gmail.com>
2023-12-14 09:17:03 +0100

    bout nico inutile

--- snake.py
+++ snake.py
@@ -4,22 +4,25 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode( (500,500) )
 
-TAILLE_CASE = 25
+BLANC,NOIR = [(255,255,255),(0,0,0)]
+
+    
+    
+    
+    
+
+    
+
 
 while True:
+
     clock.tick(1)
-
-    BLANC,NOIR = [(255,255,255),(0,0,0)]
-
     screen.fill( (16, 255, 34) )
     for i in range(0,500,TAILLE_CASE):
         for j in range(0,500,TAILLE_CASE):
             CASE = pygame.Rect(i,j,TAILLE_CASE,TAILLE_CASE)
             pygame.draw.TAILLE_CASE(screen, BLANC, CASE, 1)
-    
-    
     color = (0, 0, 255) # blue
     rect = pygame.Rect(0, 0,TAILLE_CASE , TAILLE_CASE)
 
@@ -38,18 +41,6 @@ while True:
     pygame.display.set_caption("Jeu Python")
 
 
-    
-
-    for event in pygame.event.get():
-        if event.type ==pygame.KEYDOWN:
-            if event.type == pygame.K_q:
-                pygame.quit()
-            if event.key == pygame.K_z:
-                orientation = "haut"
-        mvt(orientation)
-
-
-    pygame.display.update()
 
 
 
@@ -57,10 +48,3 @@ while True:
 
 
 
-
-
-
-
-
-
-pygame.quit()
